e7be7331396fbee46b5affb44b6154a9f9f6d7ae
Sarvar9799 <sarvaramaliy97@gmail.com>
2022-10-12 23:10:14 +0500

    we add some filed

---
+++ FirstGitProgram/src/uz/pdp/Main.java
@@ -0,0 +1,15 @@
+package uz.pdp;
+
+public class Main {
+
+    public void test(){
+        System.out.println("Test mode");
+    }
+
+    public static void voris(){
+        System.out.println("Bu voris olinayotgan method");
+    }
+    public static void main(String[] args) {
+        voris();
+    }
+}
---
+++ FirstGitProgram/src/uz/pdp/SecondProgram.java
@@ -0,0 +1,9 @@
+package uz.pdp;
+
+public class SecondProgram extends Main{
+    public static void main(String[] args) {
+        voris();
+        Main main = new Main();
+        main.test();
+    }
+}
