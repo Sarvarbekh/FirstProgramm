ba8cfdd0598a363dfe642407db2725374881d24e
Sarvar9799 <sarvaramaliy97@gmail.com>
2022-10-12 23:03:23 +0500

    we add some filed

---
+++ FirstGitProgram/src/uz/pdp/GitExample.java
@@ -0,0 +1,7 @@
+package uz.pdp;
+
+public class GitExample extends GitProgram{
+    public static void main(String[] args) {
+        System.out.println("Hello Islom pepka");
+    }
+}
--- FirstGitProgram/src/uz/pdp/GitProgram.java
+++ FirstGitProgram/src/uz/pdp/GitProgram.java
@@ -3,5 +3,6 @@ package uz.pdp;
 public class GitProgram {
     public static void main(String[] args) {
         System.out.println("Hello my first git program");
+        System.out.println("We now add some filed");
     }
 }
