f96dc0224df8098e1398e53cd88e20e6af8c8a4b
Sarvarbekh <95494467+Sarvarbekh@users.noreply.github.com>
2022-10-13 12:22:30 +0500

    Update GitExample.java

--- FirstGitProgram/src/uz/pdp/GitExample.java
+++ FirstGitProgram/src/uz/pdp/GitExample.java
@@ -1,6 +1,17 @@
 package uz.pdp;
 
 public class GitExample extends GitProgram{
+    private String name;
+    private int age;
+    
+   public GitExample(){
+   }
+    
+    public GitExample(String name, int age){
+        this.age = age;
+        this.name = name;
+    }
+    
     public static void main(String[] args) {
         System.out.println("Hello Islom pepka");
     }
